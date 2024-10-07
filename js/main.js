f284da134703f4e7ee20f5fa9a52796081a29ab5
Ivan Prosyanik <vanyaprosyanik89@gmail.com>
2024-10-07 21:55:36 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,62 @@
+
+const progressCircle = document.querySelector(".autoplay-progress");
+let headerSwiper = new Swiper(".header__swiper", {
+  effect: 'fade',
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".header__swiper-pagination",
+    type: "fraction",
+  },
+});
+
+let carsSwiper = new Swiper(".cars__swiper", {
+  slidesPerView: 2,
+  spaceBetween: 15,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
+
+const video = document.getElementById('video');
+
+function cursor(e) {
+  if (e.paused) {
+    e.play();
+    document.body.style.cursor = 'url(../images/icon-pause.svg), auto';  // Если видео на паузе, запускаем его
+  } else {
+    e.pause(); // Если видео воспроизводится, ставим на паузу
+    document.body.style.cursor = 'url(../images/icon-play.svg), auto';
+  }
+}
+
+video.addEventListener('mouseenter', () => {
+  if (video.paused) {
+    document.body.style.cursor = 'url(../images/icon-play.svg), auto';
+  }
+});
+
+video.addEventListener('mouseleave', () => {
+  document.body.style.cursor = 'default'
+})
+
+video.addEventListener('click', function () { cursor(video) });
+
+const photosSwiper = new Swiper(".photos__swiper", {
+  horizontal: true,
+  effect: 'fade',
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".photos__swiper-pagination",
+    type: "fraction",
+  },
+});
